2ce56d55a80356534e16fbbfd013065ba7374858
Utkarsh09102004 <utkarshagarwal204@gmail.com>
2024-11-10 08:04:15 +0530

    env ki bt

--- hacksCBSproject/settings.py
+++ hacksCBSproject/settings.py
@@ -137,5 +137,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jsingh12_be22@thapar.edu'
-EMAIL_HOST_PASSWORD = 'cacv afrs snru qkct'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
